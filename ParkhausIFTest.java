d330c4f769c53c72d1f35958531baccddd20dd6b
Max Ziegerer <max.ziegerer@smail.inf.h-brs.de>
2023-04-18 23:14:29 +0200

    ParkhausIF changes

--- ParkhausIFTest.java
+++ ParkhausIFTest.java
@@ -1,20 +1,25 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ParkhausIFTest {
+    Parkhaus p1;
     @BeforeEach
     void setUp() {
-
+        p1 = new Parkhaus();
     }
 
     @AfterEach
     void tearDown(){
-
+        p1 = null;
     }
 
     @Test
-    void bsptest(){
-
+    @DisplayName("Auto wird beim ziehen des parktickets zurückgegeben")
+    void pulltest(){
+        assertEquals(p1.pull().getClass(), Auto.class);
     }
 }
