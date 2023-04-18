30a611f84ba828f8e82d7341248632a2cc3c86b1
Max Ziegerer <max.ziegerer@smail.inf.h-brs.de>
2023-04-18 22:32:37 +0200

    Grundstruktur

---
+++ ParkhausIFTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ParkhausIFTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown(){
+
+    }
+
+    @Test
+    void bsptest(){
+
+    }
+}
