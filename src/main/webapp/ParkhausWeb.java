4b288bfa65a93d1e73dfa1ce76768c03e644c019
maxrzn <95301983+maxrzn@users.noreply.github.com>
2023-06-03 22:03:31 +0200

    einfahrt log hinzugefügt

--- src/main/webapp/ParkhausWeb.java
+++ src/main/webapp/ParkhausWeb.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
+import java1.Auto;
 import java1.Parkhaus;
 import java1.Zeit;
 
@@ -16,12 +17,14 @@ public class ParkhausWeb extends HttpServlet {
     private String command;
     private Parkhaus p;
     //private String log;
+    private String inLog;
 
 
     public void init() {
         message = "Parkhaus";
         p = new Parkhaus(10,0.01,new Zeit(4,4,2023,12,30));
         //log = "empty log";
+        this.inLog = "";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -43,63 +46,14 @@ public class ParkhausWeb extends HttpServlet {
         } else {
             out.println("<h1>"+this.message+"</h1>");
         }*/
-        out.println("<br>\n" +
-                "<b> aktuelle Zeit</b> " +
-                "<table>\n" +
-                "    <tr>\n" +
-                "        <td>\n" +
-                "           <form method=\"post\">\n" +
-                //"             <input type=\"hidden\" name=\"aktion\" value=\"timewarp\">\n" +
-                "               <input type=\"/text\" name=\"inputzeitsprung\" value=\""+p.getAktuelleZeit().toString()+"\">\n" +
-                "               <input type=\"submit\" value=\"Timewarp\">" +
-                "           </form>\n" +
-                "        </td>\n" +
-                "        <td>\n" +
-                "            <form method=\"post\">\n" +
-                "            <input type=\"submit\" value=\"+15 min\" name=\"input15min\">\n" +
-                "            </form>\n" +
-                "        </td>\n" +
-                "        <td>\n" +
-                "            <form method=\"post\">\n" +
-                "            <input type=\"submit\" value=\"+1 h\" name=\"input1h\">\n" +
-                "        </td>\n" +
-                "    </tr>\n" +
-                "   <tr>\n" +
-                "       <td>\n" +
-                "            <form method=\"post\">\n" +
-                "               <input type=\"submit\" value=\"auto parken\" name=\"pull\">\n" +
-                "       </td>\n" +
-                "   </tr>\n" +
-                "</table>\n" +
-                "<p>" + p.getLog() + "</p>");
-        out.println("</body></html>");
-    }
+        /*
+        out.println("<style type=\"text/css\">\n" +
+                "  td {\n" +
+                "    padding: 0 30px;\n" +
+                "  }\n" +
+                "</style>");
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
-        //Zeitsprung
-        if(request.getParameter("inputzeitsprung") != null) {
-            p.timewarp(request.getParameter("inputzeitsprung"));
-        }
-        //+15 min
-        if(request.getParameter("input15min") != null){
-            p.timeskip(0,15);
-        }
-        //+1h
-        if(request.getParameter("input1h") != null) {
-            p.timeskip(1,0);
-        }
-        //pull
-        if(request.getParameter("pull") != null) {
-            p.pull();
-        }
-
-
-        //request.setAttribute("inputzeitsprung",p.getAktuelleZeit().toString());
-        //this.zahl2 = Integer.parseInt(request.getParameter("input15min"));
-        out.println("<html><body> ");
+         */
         out.println("<br>\n" +
                         "<b> aktuelle Zeit</b> " +
                         "<table>\n" +
@@ -128,7 +82,96 @@ public class ParkhausWeb extends HttpServlet {
                         "       </td>\n" +
                         "   </tr>\n" +
                         "</table>\n" +
-                        "<p>" + p.getLog() + "</p>");
+                        "<table>\n" +
+                        "    <tr>\n" +
+                        "        <td>\n" +
+                        "<h4> Einfahrt Log </h4>" +
+                        "<p>" + this.inLog + "</p>" +
+                        "        </td>\n" +
+                        "       <td>\n" +
+                        "<h4>&nbsp;&nbsp;&nbsp; Ausfahrt Log </h4>" +
+                        "<p>" + p.getLog() + "</p>" +
+                        "       </td>\n" +
+                        "   </tr>\n" +
+                        "</table>\n"
+        );
+        out.println("</body></html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        //Zeitsprung
+        if(request.getParameter("inputzeitsprung") != null) {
+            p.timewarp(request.getParameter("inputzeitsprung"));
+        }
+        //+15 min
+        if(request.getParameter("input15min") != null){
+            p.timeskip(0,15);
+        }
+        //+1h
+        if(request.getParameter("input1h") != null) {
+            p.timeskip(1,0);
+        }
+        //pull
+        if(request.getParameter("pull") != null) {
+            Auto a = p.pull();
+            this.inLog += "&nbsp;&nbsp;&nbsp;Kennzeichen: " + a.getId() + " Timestamp: " + p.getAktuelleZeit()+ "\n<br>";
+        }
+
+
+        //request.setAttribute("inputzeitsprung",p.getAktuelleZeit().toString());
+        //this.zahl2 = Integer.parseInt(request.getParameter("input15min"));
+        out.println("<html><body> ");
+        /*out.println("<style type=\"text/css\">\n" +
+                "  td {\n" +
+                "    padding: 0 30px;\n" +
+                "  }\n" +
+                "</style>");
+
+         */
+        out.println("<br>\n" +
+                "<b> aktuelle Zeit</b> " +
+                "<table>\n" +
+                "    <tr>\n" +
+                "        <td>\n" +
+                "           <form method=\"post\">\n" +
+                //"             <input type=\"hidden\" name=\"aktion\" value=\"timewarp\">\n" +
+                "               <input type=\"/text\" name=\"inputzeitsprung\" value=\""+p.getAktuelleZeit().toString()+"\">\n" +
+                "               <input type=\"submit\" value=\"Timewarp\">" +
+                "           </form>\n" +
+                "        </td>\n" +
+                "        <td>\n" +
+                "            <form method=\"post\">\n" +
+                "            <input type=\"submit\" value=\"+15 min\" name=\"input15min\">\n" +
+                "            </form>\n" +
+                "        </td>\n" +
+                "        <td>\n" +
+                "            <form method=\"post\">\n" +
+                "            <input type=\"submit\" value=\"+1 h\" name=\"input1h\">\n" +
+                "        </td>\n" +
+                "    </tr>\n" +
+                "   <tr>\n" +
+                "       <td>\n" +
+                "            <form method=\"post\">\n" +
+                "               <input type=\"submit\" value=\"auto parken\" name=\"pull\">\n" +
+                "       </td>\n" +
+                "   </tr>\n" +
+                "</table>\n" +
+                "<table>\n" +
+                "    <tr>\n" +
+                "        <td>\n" +
+                "<h4> Einfahrt Log </h4>" +
+                "<p>" + this.inLog + "</p>" +
+                "        </td>\n" +
+                "       <td>\n" +
+                "<h4>&nbsp;&nbsp;&nbsp; Ausfahrt Log </h4>" +
+                "<p>" + p.getLog() + "</p>" +
+                "       </td>\n" +
+                "   </tr>\n" +
+                "</table>\n"
+        );
         out.println("</body></html>");
     }
 
